f1de3dc572aff11fc2838223748fd46677e9e54d
Mateus Lopes <73197343+progmateus@users.noreply.github.com>
2022-11-28 00:05:50 -0300

    meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,7 +10,6 @@ export default class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
-                    <meta httpEquiv="Content-Security-Policy" content="" />
                 </Head>
                 <body>
                     <Main />
