dc78200020340867d5be79a63ce99534ebd8b686
Mateus Lopes <73197343+progmateus@users.noreply.github.com>
2022-11-27 23:38:45 -0300

    try

--- next.config.js
+++ next.config.js
@@ -2,5 +2,22 @@
 const nextConfig = {
   reactStrictMode: true,
 }
-
 module.exports = nextConfig
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            // // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+            key: 'Content-Security-Policy',
+            value:
+              "default-src [52.90.220.102] 'self' data: 'unsafe-inline'"
+          }
+        ]
+      }
+    ]
+  }
+}
