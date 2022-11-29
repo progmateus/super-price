91dd5dc47440cb2cddee2b050f817483f2a127da
Mateus Lopes <73197343+progmateus@users.noreply.github.com>
2022-11-29 01:13:03 -0300

    fine

--- src/components/scannerModal/Scanner.tsx
+++ src/components/scannerModal/Scanner.tsx
@@ -82,7 +82,7 @@ export function Scanner(props) {
                 target: props.scannerRef.current,
                 constraints: {
                     width: 400,
-                    height: 300,
+                    height: 200,
                     facingMode: "environment",
                     /// deviceId: "7832475934759384534"
                 },
