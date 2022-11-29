51f2bc20eaa65ec93b65ecb4afe6702b84a1711d
Mateus Lopes <73197343+progmateus@users.noreply.github.com>
2022-11-29 01:37:21 -0300

    fixing

--- src/components/scannerModal/Scanner.tsx
+++ src/components/scannerModal/Scanner.tsx
@@ -81,7 +81,8 @@ export function Scanner(props) {
                 type: "LiveStream",
                 target: props.scannerRef.current,
                 constraints: {
-
+                    width: 250,
+                    height: 250,
                     facingMode: "environment",
                     /// deviceId: "7832475934759384534"
                 },
