2775fcf5b1f02f002cd36708eec4bb335acf7111
Mateus Lopes <73197343+progmateus@users.noreply.github.com>
2023-02-03 20:35:38 -0300

    added google adsense

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,8 @@ export default class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1725565658206528"
+                        crossOrigin="anonymous"></script>
                 </Head>
                 <body>
                     <Main />
